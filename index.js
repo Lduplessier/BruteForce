9bdd3b6ebe1ee08368e070807b9d394ab742d53e
LDU <lucas.duplessier@gmail.com>
2020-01-10 18:35:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,14 +9,12 @@ function Check() {
     for (var no=0;no<tabc.length;no++) {
         checksum=tabc[no];
         var password="00000";
-        var nblog=login.length;
         var nbpass=password.length;
         var sum=1;
         var n=Math.max(nblog,nbpass)
         for (var i=0;i<n;i++) {
-            var index1=tab.indexOf(login.substring(i,i+1))+10;
-            var index2=tab.indexOf(password.substring(i,i+1))+10;
-            sum=sum+(index1*n*(i+1))*(index2*(i+1)*(i+1));
+            var index=tab.indexOf(password.substring(i,i+1))+10;
+            sum=sum+(index*(i+1)*(i+1));
         }
         if (sum==checksum) {
             alert("password found");
@@ -26,7 +24,9 @@ function Check() {
     }
     if (ok==0) {
         total_essai++;
-        }
+    }else{
+        alert(index);
+    }
 }
 
 function Verifie() {
